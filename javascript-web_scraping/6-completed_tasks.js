3b6ccc04770d4cbb8e657b66e435386c26b7130f
Jhony <Jhonny14_1@hotmail.com>
2024-04-06 10:17:50 -0500

    feat(6-completed_tasks): Cuenta las tareas completadas por id.

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+// How many completed?
+
+const request = require('request');
+const url = process.argv[2];
+
+if (!url) {
+  console.log('Ingrese una URL');
+  process.exit(1);
+}
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.log('Houston tenemos un problema');
+    process.exit(1);
+  }
+
+  const todos = JSON.parse(body);
+  const todosCompleted = {};
+  
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (todosCompleted[todo.userId]) {
+        todosCompleted[todo.userId]++;
+      } else {
+        todosCompleted[todo.userId] = 1;
+      }
+    }
+  });
+  console.log(todosCompleted);
+});
