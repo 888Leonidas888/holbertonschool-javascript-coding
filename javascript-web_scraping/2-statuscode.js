031a59e2bf65bc1d669ed17b5d938eb0d9baebdc
Jhony <Jhonny14_1@hotmail.com>
2024-04-06 10:12:08 -0500

    feat(2-statuscode): Realiza un petición y GET y muestra el status.

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Status code
+
+const request = require('request');
+const url = process.argv[2];
+
+if (!url) {
+  console.log('Debe proporcionar un URL');
+  process.exit(1);
+}
+
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`code: ${response.statusCode}`);
+});
