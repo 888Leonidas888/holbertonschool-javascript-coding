fdc7e6e069d1f9fc82b51bacf16b48fe784cfd0b
Jhony <Jhonny14_1@hotmail.com>
2024-04-06 22:49:28 -0500

    feat(100-starwars_character): Lista los personajes.

---
+++ javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,39 @@
+const request = require('request');
+const idMovie = process.argv[2];
+
+if (!idMovie) {
+  console.log('Ingrese un id del 1 al 7');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.hbtn.io/api/films/${idMovie}/`;
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  } else {
+    if (response.statusCode === 200) {
+      const peoples = JSON.parse(body);
+      const n = [];
+      n.push(...peoples.characters);
+      n.forEach((urlPeople) => {
+        request(urlPeople, function (err, response, body) {
+          if (err) {
+            console.log('Error al consultar personaje');
+            process.exit(1);
+          } else {
+            if (response.statusCode === 200) {
+              const character = JSON.parse(body);
+              console.log(character.name);
+            } else {
+              console.log('Houston tenemos otro problema');
+              process.exit(1)
+            }
+          }
+        });
+      });
+    } else {
+      console.log('Houston tenemos un problema');
+    }
+  }
+});
