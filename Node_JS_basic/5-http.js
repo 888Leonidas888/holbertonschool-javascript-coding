d96e4370a133774f421893f0e42548abeaed9e0a
Jhony <Jhonny14_1@hotmail.com>
2024-05-04 23:08:04 -0500

    chore(5-http): se reemplaza salto de línea.

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -53,7 +53,7 @@ function countStudents(filePath) {
       if (err) {
         reject(new Error('Cannot load the database'));
       } else {
-        const persons = data.split('\r\n').filter((line) => line.trim() !== '');
+        const persons = data.split('\n').filter((line) => line.trim() !== '');
         resolve(stats(persons));
       }
     });
